e7269d2d74c406a4c2538b4e0dfe6176c53f0de7
Ovidiu Mircescu <ovidiu.mircescu@edf.fr>
2023-03-12 16:22:15 +0100

    Using tasks.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,18 @@
 use futures::future::join_all;
 use futures::future::FutureExt;
 
-async fn foo(i: u32) -> u32 {
+async fn bar(i: u32){
+    std::thread::sleep(std::time::Duration::from_secs(i.into()));
+    // async_std::task::sleep(std::time::Duration::from_secs(i.into())).await;
     println!("{i}");
-     i
+}
+
+async fn foo(i: u32) -> u32 {
+    // std::thread::sleep(std::time::Duration::from_secs(i.into()));
+    // async_std::task::sleep(std::time::Duration::from_secs(i.into())).await;
+    // println!("{i}");
+    async_std::task::spawn(bar(i)).await;
+    i
 }
 
 #[cfg(test)]
@@ -12,8 +21,9 @@ mod tests {
 
     #[async_std::test]
     async fn it_works() {
-        let futures = vec![foo(1), foo(2), foo(3)];
-        assert_eq!(join_all(futures).await, [1, 2, 3]);
+        let futures = vec![foo(3), foo(2), foo(1)];
+        assert_eq!(join_all(futures).await, [3, 2, 1]);
+        // futures::join!(foo(3), foo(2), foo(1));
         let f1 = foo(1);
         let shared1 = f1.shared();
         let shared2 = shared1.clone();
