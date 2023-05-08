241375e03ac5362b1ed14f86180b7c6b50b572f7
Ovidiu Mircescu <ovidiu.mircescu@edf.fr>
2023-05-08 12:11:53 +0200

    Generic future.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use async_process::Command;
-use futures::future::{self, Shared};
+use futures::future;
 use futures::future::FutureExt;
+use core::pin::Pin;
 
 pub async fn thread_sleep(i: u32){
     std::thread::sleep(std::time::Duration::from_secs(i.into()));
@@ -35,6 +36,14 @@ where
   Fut: future::Future<Output = Result<u32, String>>
 {
     let sync_value = args.await?;
+    s_command_sleep(sync_value).await
+}
+
+// run_command is template & generates different types of futures which cannot be mixed in a collection.
+// ref_run_command generates futures that can be mixed in a collection.
+pub async fn ref_run_command(args:Pin<Box<dyn futures::Future<Output = Result<u32, String>> + Send >>) -> Result<u32, String>
+{
+    let sync_value = args.boxed().await?;
     s_command_sleep(sync_value).await
 }
 
@@ -93,11 +102,34 @@ mod tests {
     #[async_std::test]
     async fn mini_graph() {
         let start_time = Instant::now();
-        let r1 = s_command_sleep(1).shared();
+        let r1 = run_command(async{Ok(1)}).shared();
         let r2 = run_command(r1.clone()).shared();
         let r3 = run_command(r1.clone()).shared();
         let r4 = run_command(r2.clone()).shared();
-        future::join(future::join(future::join(r1, r2), r3), r4).await;
+        let results = future::join4(r1, r2, r3, r4).await;
+        // let lst: Vec<_> = vec![r1, r2, r3, r4]; // compile error!
+        // let results = future::join_all(lst);
+
+        // future::join(future::join(future::join(r1, r2), r3), r4).await; // good!
+        let run_time = start_time.elapsed().as_secs();
+        // println!("run_time:{run_time}s");
+        assert!(run_time == 3);
+    }
+
+    #[async_std::test]
+    async fn list_fut() {
+        let start_time = Instant::now();
+        let r1 = ref_run_command(Box::pin(async{Ok(1)})).shared();
+        let r2 = ref_run_command(Box::pin(r1.clone())).shared();
+        let r3 = ref_run_command(Box::pin(r1.clone())).shared();
+        let r4 = ref_run_command(Box::pin(r2.clone())).shared();
+        let lst: Vec<_> = vec![r1, r2, r3, r4];
+        let results = future::join_all(lst).await;
+
+        for res in results{
+            res.map_err(|x| println!("Error: {x}")).ok();
+        }
+
         let run_time = start_time.elapsed().as_secs();
         // println!("run_time:{run_time}s");
         assert!(run_time == 3);
